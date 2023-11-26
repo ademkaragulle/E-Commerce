49026ebcfee3260984cb112f3c83702418b0a5bb
ademkaragulle <ademkaragulle@outlook.com>
2023-11-27 00:18:54 +0300

    add header and body components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header/Header'
+import { Route, Routes } from 'react-router-dom'
+import Body from './components/body/Body'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <div className='App'>
-
+      <Header />
+      <Body />
     </div>
   )
 }
