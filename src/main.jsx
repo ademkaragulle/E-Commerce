ac67e83716005b5a067e976987b8bafc30daa5fd
ademkaragulle <ademkaragulle@outlook.com>
2023-11-27 22:51:43 +0300

    change main.jsx for redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import './index.css'
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
-import store from './components/store/store.jsx';
 
+import { store } from './components/store/Store.jsx'
 
 const root = createRoot(document.getElementById("root"));
 
