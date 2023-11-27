2a5746b0f2fdefc3e61bd27c650ce919aa600524
ademkaragulle <ademkaragulle@outlook.com>
2023-11-27 22:53:32 +0300

    add redux and delete product object

--- src/components/body/products/Products.jsx
+++ src/components/body/products/Products.jsx
@@ -1,100 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Products.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProduct } from '../../store/slices/ProductSlice'
 
 function Products() {
 
-    const products = [
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
-        },
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
+    const { data, loading, error, currentcategory } = useSelector((store) => {
+        return {
+            currentPage: store.currenCategory.currentcategory,
+            data: store.product.data,
+            loading: store.product.loading,
+            error: store.product.error,
+        }
+    })
+    console.log(currentcategory)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getProduct(currentcategory))
+    }, [dispatch, currentcategory])
 
-        },
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
-        },
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
-        },
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
-        },
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
-        },
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
-        },
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
-        },
-        {
-            productId: 1,
-            prodductName: "iphone 11",
-            productUrl: "https://st-troy.mncdn.com/mnresize/1500/1500/Content/media/ProductImg/original/mhdc3tua-apple-iphone-11-64gb-beyaz-mhdc3tua-637610877233906011.jpg",
-            producrDesc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quo voluptate soluta libero id mollitia officiis distinctio illum, aliquid ipsum dicta! Placeat aspernatur vitae amet voluptas excepturi similique quibusdam ducimus?",
-            productScore: 3.8,
-            productPrice: 1800
-        },
-    ]
 
     return (
         <div id='products' className='row g-3'>
-            {products.map((product, index) => (
+            {data && data.map((product, index) => (
                 <div key={index} className="col-4">
-                    <div className='card card-relative'>
-                        <a className="product-fav text-dark">
+                    <div style={{ minHeight: "464px" }} className='card card-relative my-auto'>
+                        <a className="product-fav text-dark bg-light">
                             <i className="bi bi-heart"></i>
                         </a>
-                        <div className="card-img">
-                            <img className='w-100' src={product.productUrl} alt="" />
+                        <div style={{ height: "250px", overflow: "hidden" }} className="card-img">
+                            <img style={{ height: "100%" }} src={product.images[0]} alt="" />
                         </div>
                         <div className="card-body">
-                            <h3><a className='text-dark text-decoration-none' href="">{product.prodductName}</a></h3>
-                            <span>Score: {product.productScore} <i className="bi bi-star-fill text-warning"></i></span><br />
-                            <span>Price: {product.productPrice} $</span><br />
-                            <div className="btn btn-primary mt-3 form-control">Add to cart</div>
+                            <h5><a className='text-dark text-decoration-none ' href="">{product.title}</a></h5>
+                            <span>Score: {product.id} <i className="bi bi-star-fill text-warning"></i></span><br />
+                            <span>Price: {product.price} $</span><br />
+                        </div>
+                        <div className='px-3'>
+                            <div className="btn btn-primary form-control my-0 mb-4">Add to cart</div>
                         </div>
                     </div>
                 </div>
